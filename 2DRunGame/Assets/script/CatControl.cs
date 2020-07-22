a8c079b76b30257071647d2b36976a1d2cac807d
SeanGau <rrtw0627@gmail.com>
2020-07-22 19:43:39 +0800

    learn some scripts

---
+++ 2DRunGame/Assets/script/CatControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform trans;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.position += new Vector3(0.1f, 0, 0);
+    }
+}
--- 2DRunGame/Assets/script/car.cs
+++ 2DRunGame/Assets/script/car.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 
-public class car : MonoBehaviour
+public class Car : MonoBehaviour
 {
-    string brand;
-    int cc_mount;
-    float speed, weight;
-    bool hasSunroof;
-    Color color;
+    [Tooltip("汽車品牌")]
+    public string brand = "Suzuki";
+    [Tooltip("CC數"), Range(0, 9999)]
+    public int ccAmount = 1000;
+    [Range(0, 200)]
+    public float speed = 60.5f;
+    [Tooltip("重量(kg)"), Range(0, 9999)]
+    public float weight = 980f;
+    [Tooltip("是否有天窗")]
+    public bool hasSunroof = false;
+    public Color color = new Color32(100,100,255,255);
 }
