315dfe7ca6e34c137aed5bdb0a27c7bcbb8ea610
SeanGau <rrtw0627@gmail.com>
2020-07-27 21:29:08 +0800

    add player Control functions

---
+++ 2DRunGame/Assets/script/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Tooltip("要追蹤的物件")]
+    public Transform target;
+    [Tooltip("追蹤速度"), Range(1, 100)]
+    public float speed = 10;
+    [Tooltip("追蹤上限與下限：X 為下方限制，Y 為上方限制")]
+    public Vector2 limit = new Vector2(0, 5);
+    public Vector2 offset = new Vector2(5, 0);
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 pos = target.position + (Vector3)offset;
+        pos.z = -10;
+        pos.y = Mathf.Clamp(pos.y, limit.x, limit.y);
+
+        transform.position = Vector3.Lerp(transform.position, pos, 0.5f * Time.deltaTime * speed);
+    }
+}
--- 2DRunGame/Assets/script/CatControl.cs
+++ 2DRunGame/Assets/script/CatControl.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class CatControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform trans;
+    #region 資料欄位
+    [Tooltip("移動速度"), Range(0, 100)]
+    public float movingSpeed = 1f;
+    [Tooltip("跳躍高度"), Range(0, 1000)]
+    public int jumpHeight = 300;
+    [Tooltip("血量"), Range(0, 1000)]
+    public float hp = 500;    
+    [Tooltip("中心點位移")]
+    public Vector3 offset = new Vector3(0,-1.2f,0);
+
+
+    [Header("音效")]
+    public AudioClip hurtAudio;
+    public AudioClip slideAudio;
+    public AudioClip jumpAudio;
+    public AudioClip getAudio;
+
+    bool isTouchingGround = false;
+
+    int moneyGet;
+    Animator _animator;
+    Rigidbody2D _rigidbody;
+    CapsuleCollider2D _collider;
+    #endregion
+
     void Start()
     {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>();
+        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        trans.position += new Vector3(0.1f, 0, 0);
+        Slide();
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Slide()
+    {
+        if (isTouchingGround && Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _animator.SetBool("isSliding", true);
+        }
+        else
+        {
+            _animator.SetBool("isSliding", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (isTouchingGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            isTouchingGround = false;
+            _rigidbody.AddForce(transform.up * jumpHeight);
+        }
+    }
+
+    void Move()
+    {
+        _animator.SetBool("isMoving", true);
+        _animator.SetBool("isJumping", !isTouchingGround);
+        transform.Translate(transform.right * movingSpeed / 10);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "地板")
+        {
+            isTouchingGround = true;
+        }
     }
 }
